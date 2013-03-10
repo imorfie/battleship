293de40f7274e78fa7b7bfc99b87e5595ee42159
Rob Richard <robrichard87@gmail.com>
2013-03-10 00:51:17 -0500

    Added some error checking
    Signed-off-by: Rob Richard <robrichard87@gmail.com>

--- spec/battleship_spec.js
+++ spec/battleship_spec.js
@@ -2,11 +2,43 @@
 (function () {
   "use strict";
 
-  describe("Ship", function() {
-    it("should take hit", function() {
-      var ship = new Ship(3);
+  describe("Ship", function () {
+    describe("Creation error handling", function() {
+      it("should throw if size is not greater than one", function() {
+        expect(function () { var ship = new tddjs.battleship.Ship(-1); }).toThrow();
+        expect(function () { var ship = new tddjs.battleship.Ship(0); }).toThrow();
+        expect(function () { var ship = new tddjs.battleship.Ship({}); }).toThrow();
+      });      
+    });
 
-      expect(function () { ship.hit(0); }).not.toThrow();;
+    it("should take hit", function () {
+      var ship = new tddjs.battleship.Ship(3);
+
+      expect(function () { ship.hit(0); }).not.toThrow();
+    });
+
+    it("should throw if hit index is not in ship size", function() {
+      var ship = new tddjs.battleship.Ship(3);
+
+      expect(function () { ship.hit(4); }).toThrow();
+      expect(function () { ship.hit(-1); }).toThrow();
+    });
+
+    it("should report sunk when all positions are hit", function () {
+      var ship = new tddjs.battleship.Ship(3);
+      ship.hit(0);
+      ship.hit(1);
+      ship.hit(2);
+
+      expect(ship.isSunk()).toBeTruthy();
+    });
+
+    it("should report not sunk when some positions are hit", function () {
+      var ship = new tddjs.battleship.Ship(3);
+      ship.hit(0);
+      ship.hit(2);
+
+      expect(ship.isSunk()).toBeFalsy();
     });
   });
 
--- src/battleship.js
+++ src/battleship.js
@@ -1,3 +1,4 @@
+/*global tddjs*/
 /*function Board() {
   var map = new Array(10);
   for (var i = 0; i < map.length; i += 1) {
@@ -5,19 +6,44 @@
   }
 }
 */
+tddjs.namespace("battleship");
 (function () {
   "use strict";
 
   function Ship(length) {
     var i;
+
+    if ((typeof length !== "number") || length < 1) {
+      throw new Error("length is less than 1");
+    }
+
     this.spots = [];
-    for (i = 0; i < length; i += 0) {
+    for (i = 0; i < length; i += 1) {
       this.spots[i] = false;
     }
   }
 
   Ship.prototype.hit = function (position) {
+    if ((typeof position !== "number") || position < 0) {
+      throw new Error("position is less than 1");
+    }
+    if (position >= this.spots.length) {
+      throw new Error("Hit position is larger than ship");
+    }
     this.spots[position] = true;
   };
 
+  Ship.prototype.isSunk = function () {
+    var i;
+    for (i = 0; i < this.spots.length; i += 1) {
+      if (this.spots[i] !== true) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  tddjs.battleship.Ship = Ship;
+
 }());
+
